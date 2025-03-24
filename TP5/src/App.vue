f8fc151ecf9abfd241e74c11cb1566cebafab0a4
ayabenjelloun <147836916+Benaya2005@users.noreply.github.com>
2025-03-24 02:37:27 +0100

    Add files via upload

---
+++ TP5/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="nav_bar">
+      <router-link to="/">Home</router-link>
+      <router-link to="/add">Ajouter un emploi</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<style>
+body {
+  margin: 0;
+  font-family: 'Arial', sans-serif;
+  background-color: #f5f5f5;
+}
+
+#app {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.nav_bar {
+  background-color:rgb(22, 114, 114);
+  padding: 15px 20px;
+  display: flex;
+  gap: 20px;
+  justify-content: flex-start;
+}
+
+.nav_bar > a {
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+  font-size: 16px;
+}
+
+.nav_bar > a:hover {
+  color: #25d1dd;
+}
+
+</style>
